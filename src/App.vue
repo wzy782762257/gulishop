a5d15f5d111e685793bfa9eac94b90c4b232e7b5
wzy782762257 <wzy782762257@126.com>
2021-08-07 19:29:16 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+<Header></Header>
+<Footer></Footer>
+</div>
+</template>
+
+<script>
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+  export default {
+    name:"",
+    components:{
+      Header,
+      Footer
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+
+</style>
